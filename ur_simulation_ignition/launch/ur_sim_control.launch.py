f011e00bfeec6f93af22303340e705ab7139977d
Denis Štogl <denis@stogl.de>
2022-01-27 16:16:44 +0100

    Use moveit_config instead ur_bringup package.

--- ur_simulation_ignition/launch/ur_sim_control.launch.py
+++ ur_simulation_ignition/launch/ur_sim_control.launch.py
@@ -242,7 +242,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "runtime_config_package",
-            default_value="ur_bringup",
+            default_value="ur_simulation_ignition",
             description='Package with the controller\'s configuration in "config" folder. \
         Usually the argument is not set, it enables use of a custom setup.',
         )
