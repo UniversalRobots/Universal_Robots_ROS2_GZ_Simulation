UniversalRobots/Universal_Robots_ROS2_GZ_Simulation

commit 858a5427931661cd544a6997244a2327b7137b0a

Adding more launch arguments (backport of #37)
* Added world_file as launch argument
* Added gazebo_gui launch argument

Co-authored-by: Vincenzo Di Pentima <DiPentima@fzi.de>

ur_simulation_gz/launch/ur_sim_control.launch.py
@@ -38,7 +38,13 @@ from launch.actions import (
 from launch.conditions import IfCondition, UnlessCondition
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+    IfElseSubstitution,
+)
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
@@ -58,6 +64,8 @@ def launch_setup(context, *args, **kwargs):
     start_joint_controller = LaunchConfiguration("start_joint_controller")
     initial_joint_controller = LaunchConfiguration("initial_joint_controller")
     launch_rviz = LaunchConfiguration("launch_rviz")
+    gazebo_gui = LaunchConfiguration("gazebo_gui")
+    world_file = LaunchConfiguration("world_file")
 
     initial_joint_controllers = PathJoinSubstitution(
         [FindPackageShare(runtime_config_package), "config", controllers_file]
@@ -165,7 +173,13 @@ def launch_setup(context, *args, **kwargs):
         PythonLaunchDescriptionSource(
             [FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
         ),
-        launch_arguments={"gz_args": " -r -v 4 empty.sdf"}.items(),
+        launch_arguments={
+            "gz_args": IfElseSubstitution(
+                gazebo_gui,
+                if_value=[" -r -v 4 ", world_file],
+                else_value=[" -s -r -v 4 ", world_file],
+            )
+        }.items(),
     )
 
     nodes_to_start = [
@@ -269,6 +283,18 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz?")
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gazebo_gui", default_value="true", description="Start gazebo with GUI?"
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "world_file",
+            default_value="empty.sdf",
+            description="Gazebo world file (absolute path or filename from the gazebosim worlds collection) containing a custom world.",
+        )
     )
 
     return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])

commit 01ccb2f5c1e1f52faa2651c38bdb5689a9b56641

Replace IfElseSubstitution with IfCondition UnlessCondition (#44)
This fixes a regression introduced in 858a5427931661cd544a6997244a2327b7137b0a

ur_simulation_gz/launch/ur_sim_control.launch.py
@@ -43,7 +43,6 @@ from launch.substitutions import (
     FindExecutable,
     LaunchConfiguration,
     PathJoinSubstitution,
-    IfElseSubstitution,
 )
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
@@ -168,18 +167,24 @@ def launch_setup(context, *args, **kwargs):
             "true",
         ],
     )
-
-    gz_launch_description = IncludeLaunchDescription(
+    gz_launch_description_with_gui = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
         ),
         launch_arguments={
-            "gz_args": IfElseSubstitution(
-                gazebo_gui,
-                if_value=[" -r -v 4 ", world_file],
-                else_value=[" -s -r -v 4 ", world_file],
-            )
+            "gz_args": ["-r", "-v", "4", world_file]
         }.items(),
+        condition=IfCondition(gazebo_gui)
+    )
+
+    gz_launch_description_without_gui = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
+        ),
+        launch_arguments={
+            "gz_args": ["-s", "-r", "-v", "4", world_file]
+        }.items(),
+        condition=UnlessCondition(gazebo_gui)
     )
 
     nodes_to_start = [
@@ -189,7 +194,8 @@ def launch_setup(context, *args, **kwargs):
         initial_joint_controller_spawner_stopped,
         initial_joint_controller_spawner_started,
         gz_spawn_entity,
-        gz_launch_description,
+        gz_launch_description_with_gui,
+        gz_launch_description_without_gui,
     ]
 
     return nodes_to_start