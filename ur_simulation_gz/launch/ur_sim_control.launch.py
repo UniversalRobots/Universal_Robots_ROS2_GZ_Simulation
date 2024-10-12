a038c26ebae899e895d0be2d6caf0e45a33d65c1
Felix Exner <exner@fzi.de>
2024-10-12 14:01:22 +0200

    Add gz_bridge for /clock topic

--- ur_simulation_gz/launch/ur_sim_control.launch.py
+++ ur_simulation_gz/launch/ur_sim_control.launch.py
@@ -169,6 +169,16 @@ def launch_setup(context, *args, **kwargs):
         }.items(),
     )
 
+    # Make the /clock topic available in ROS
+    gz_sim_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+        ],
+        output="screen",
+    )
+
     nodes_to_start = [
         robot_state_publisher_node,
         joint_state_broadcaster_spawner,
@@ -177,6 +187,7 @@ def launch_setup(context, *args, **kwargs):
         initial_joint_controller_spawner_started,
         gz_spawn_entity,
         gz_launch_description,
+        gz_sim_bridge,
     ]
 
     return nodes_to_start
