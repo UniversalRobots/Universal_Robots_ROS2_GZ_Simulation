d3cd68edec4784771a39f816596b6fb116839692
Felix Exner <feex@universal-robots.com>
2025-02-05 09:55:18 +0100

    Add support for UR7e and UR12e

--- ur_simulation_gz/launch/ur_sim_moveit.launch.py
+++ ur_simulation_gz/launch/ur_sim_moveit.launch.py
@@ -83,7 +83,19 @@ def generate_launch_description():
         DeclareLaunchArgument(
             "ur_type",
             description="Type/series of used UR robot.",
-            choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20", "ur30"],
+            choices=[
+                "ur3",
+                "ur3e",
+                "ur5",
+                "ur5e",
+                "ur7e",
+                "ur10",
+                "ur10e",
+                "ur12e",
+                "ur16e",
+                "ur20",
+                "ur30",
+            ],
             default_value="ur5e",
         )
     )
